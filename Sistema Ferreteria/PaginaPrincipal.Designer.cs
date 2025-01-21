f2f7aab502f9740e9037bf112fa680f2a753d92e
Bayardo <bayardogarciaqxfx@gmail.com>
2025-01-21 00:01:03 -0600

    Transformación de Login de ventana primaria a ventana por MDI

--- Sistema Ferreteria/PaginaPrincipal.Designer.cs
+++ Sistema Ferreteria/PaginaPrincipal.Designer.cs
@@ -28,13 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PaginaPrincipal));
             this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
             this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
             this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
             this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
             this.ribbonStatusBar1 = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
             this.ribbonPage2 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+            this.btnLogin = new DevExpress.XtraBars.BarButtonItem();
+            this.btnLogout = new DevExpress.XtraBars.BarButtonItem();
+            this.unitOfWork = new DevExpress.Xpo.UnitOfWork(this.components);
+            this.slblUsuario = new DevExpress.XtraBars.BarStaticItem();
             ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork)).BeginInit();
             this.SuspendLayout();
             // 
             // ribbonControl1
@@ -42,13 +49,16 @@
             this.ribbonControl1.ExpandCollapseItem.Id = 0;
             this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
             this.ribbonControl1.ExpandCollapseItem,
-            this.ribbonControl1.SearchEditItem});
+            this.ribbonControl1.SearchEditItem,
+            this.btnLogin,
+            this.btnLogout,
+            this.slblUsuario});
             this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
-            this.ribbonControl1.MaxItemId = 1;
+            this.ribbonControl1.MaxItemId = 5;
             this.ribbonControl1.Name = "ribbonControl1";
             this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
             this.ribbonPage1});
-            this.ribbonControl1.Size = new System.Drawing.Size(531, 194);
+            this.ribbonControl1.Size = new System.Drawing.Size(844, 194);
             this.ribbonControl1.StatusBar = this.ribbonStatusBar1;
             // 
             // ribbonPage1
@@ -57,12 +67,14 @@
             this.ribbonPageGroup1,
             this.ribbonPageGroup2});
             this.ribbonPage1.Name = "ribbonPage1";
-            this.ribbonPage1.Text = "ribbonPage1";
+            this.ribbonPage1.Text = "Usuario";
             // 
             // ribbonPageGroup1
             // 
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnLogin);
+            this.ribbonPageGroup1.ItemLinks.Add(this.btnLogout);
             this.ribbonPageGroup1.Name = "ribbonPageGroup1";
-            this.ribbonPageGroup1.Text = "ribbonPageGroup1";
+            this.ribbonPageGroup1.Text = "Sesion Usuario";
             // 
             // ribbonPageGroup2
             // 
@@ -71,26 +83,51 @@
             // 
             // ribbonStatusBar1
             // 
-            this.ribbonStatusBar1.Location = new System.Drawing.Point(0, 253);
+            this.ribbonStatusBar1.ItemLinks.Add(this.slblUsuario);
+            this.ribbonStatusBar1.Location = new System.Drawing.Point(0, 410);
             this.ribbonStatusBar1.Name = "ribbonStatusBar1";
             this.ribbonStatusBar1.Ribbon = this.ribbonControl1;
-            this.ribbonStatusBar1.Size = new System.Drawing.Size(531, 32);
+            this.ribbonStatusBar1.Size = new System.Drawing.Size(844, 32);
             // 
             // ribbonPage2
             // 
             this.ribbonPage2.Name = "ribbonPage2";
             this.ribbonPage2.Text = "ribbonPage2";
             // 
-            // MainMenu
+            // btnLogin
+            // 
+            this.btnLogin.Caption = "Iniciar Sesión";
+            this.btnLogin.Id = 2;
+            this.btnLogin.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.ImageOptions.Image")));
+            this.btnLogin.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.ImageOptions.LargeImage")));
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.btnLogin_ItemClick);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Caption = "Cerrar Sesión";
+            this.btnLogout.Id = 3;
+            this.btnLogout.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.ImageOptions.Image1")));
+            this.btnLogout.ImageOptions.LargeImage = ((System.Drawing.Image)(resources.GetObject("barButtonItem1.ImageOptions.LargeImage1")));
+            this.btnLogout.Name = "btnLogout";
+            // 
+            // slblUsuario
+            // 
+            this.slblUsuario.Caption = "barStaticItem1";
+            this.slblUsuario.Id = 4;
+            this.slblUsuario.Name = "slblUsuario";
+            // 
+            // PaginaPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 285);
+            this.ClientSize = new System.Drawing.Size(844, 442);
             this.Controls.Add(this.ribbonControl1);
             this.Controls.Add(this.ribbonStatusBar1);
-            this.Name = "MainMenu";
+            this.Name = "PaginaPrincipal";
             this.Text = "MainMenu";
             ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,5 +141,9 @@
         private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
         private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar1;
         private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage2;
+        private DevExpress.XtraBars.BarButtonItem btnLogin;
+        private DevExpress.XtraBars.BarButtonItem btnLogout;
+        private DevExpress.Xpo.UnitOfWork unitOfWork;
+        private DevExpress.XtraBars.BarStaticItem slblUsuario;
     }
 }
--- Sistema Ferreteria/PaginaPrincipal.cs
+++ Sistema Ferreteria/PaginaPrincipal.cs
@@ -1,4 +1,7 @@
-﻿using DevExpress.XtraEditors;
+﻿using DevExpress.Xpo;
+using DevExpress.XtraEditors;
+using Sistema_Ferreteria.Database;
+using Sistema_Ferreteria.Formularios;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -17,6 +20,31 @@ namespace Sistema_Ferreteria
         {
             InitializeComponent();
         }
+
         internal static int idUsuario;
+
+        private void btnLogin_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            frmLogin frm = new frmLogin(); //Se crea una instancia del formulario de login
+            DialogResult result = frm.ShowDialog(); //Se muestra el formulario de login
+
+            if (result == DialogResult.OK) //Si el resultado del formulario de login es OK
+            {
+                if (idUsuario != 0)
+                {
+                    Usuarios usuario = (Usuarios)unitOfWork.GetObjectByKey<Usuarios>(idUsuario);
+                    slblUsuario.Caption = "Usuario: " + usuario.nombre;
+                    btnLogout.Enabled = true; //Activa el boton para cerrar sesion
+                    btnLogin.Enabled = false; //Desactiva el boton para iniciar sesion
+
+                    if (usuario.rol.idRol == 1)
+                        ActivarUsuarioAdmin();
+                    else if (usuario.rol.idRol == 2)
+                        ActivarUsuarioGerencia();
+                    else
+                        ActivarUsuarioVentas();
+                }
+            }
+        }
     }
 }
